7a6f2d8235156835e1666a1f734bbbc38f0bbfff
Thai Vin <thaivinnguyen2015@gmail.com>
2018-06-12 23:28:28 +0700

    delete

--- pr2_robot_project/build/RoboND-Perception-Project/pr2_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinnt/catkin_ws/src/RoboND-Perception-Project/pr2_moveit
-# Build directory: /home/vinnt/catkin_ws/build/RoboND-Perception-Project/pr2_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- pr2_robot_project/build/project2/kuka_arm/catkin_generated/kuka_arm-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(kuka_arm_MESSAGE_FILES "")
-set(kuka_arm_SERVICE_FILES "/home/vinnt/catkin_ws/src/project2/kuka_arm/srv/CalculateIK.srv")
