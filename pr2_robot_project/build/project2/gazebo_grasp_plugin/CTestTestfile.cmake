118dab5628a3111a0707ca537b957637145743fb
Thai Vin <thaivinnguyen2015@gmail.com>
2018-06-13 01:02:01 +0700

    Upload project

---
+++ pr2_robot_project/build/project2/gazebo_grasp_plugin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vinnt/catkin_ws/src/project2/gazebo_grasp_plugin
+# Build directory: /home/vinnt/catkin_ws/build/project2/gazebo_grasp_plugin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ pr2_robot_project/devel/share/pr2_robot/cmake/pr2_robot-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(pr2_robot_MESSAGE_FILES "")
+set(pr2_robot_SERVICE_FILES "/home/vinnt/catkin_ws/src/RoboND-Perception-Project/pr2_robot/srv/PickPlace.srv;/home/vinnt/catkin_ws/src/RoboND-Perception-Project/pr2_robot/srv/Grasp.srv")
