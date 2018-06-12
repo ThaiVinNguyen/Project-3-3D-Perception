0bbb3feaee6ef4df9b59de2637ce3780eb7a4010
Thai Vin <thaivinnguyen2015@gmail.com>
2018-06-13 01:21:36 +0700

    Delete

--- pr2_robot_project/build/project2/gazebo_grasp_plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinnt/catkin_ws/src/project2/gazebo_grasp_plugin
-# Build directory: /home/vinnt/catkin_ws/build/project2/gazebo_grasp_plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
