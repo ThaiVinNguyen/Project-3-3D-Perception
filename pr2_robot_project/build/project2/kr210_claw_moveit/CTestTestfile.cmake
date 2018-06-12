d8fee9de0df5c86e7d76a5e6aec79c6e0148aa08
Thai Vin <thaivinnguyen2015@gmail.com>
2018-06-13 00:58:53 +0700

    Delete

--- pr2_robot_project/build/project2/kr210_claw_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vinnt/catkin_ws/src/project2/kr210_claw_moveit
-# Build directory: /home/vinnt/catkin_ws/build/project2/kr210_claw_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
